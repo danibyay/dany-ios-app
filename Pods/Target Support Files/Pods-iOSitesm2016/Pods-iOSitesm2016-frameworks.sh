d0c042578fa6640e892716b555b6259ded0c5ffb
Daniela Becerra <madajiji@gmail.com>
2016-03-23 14:37:37 -0600

    remove one pod dependency

--- Pods/Target Support Files/Pods-iOSitesm2016/Pods-iOSitesm2016-frameworks.sh
+++ Pods/Target Support Files/Pods-iOSitesm2016/Pods-iOSitesm2016-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-iOSitesm2016/GradientCircularProgress.framework"
-  install_framework "Pods-iOSitesm2016/MRProgress.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-iOSitesm2016/GradientCircularProgress.framework"
-  install_framework "Pods-iOSitesm2016/MRProgress.framework"
 fi
